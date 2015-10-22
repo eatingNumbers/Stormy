acec8662f8c712acbd6740f8508a5c6bbefc9cdf
eatingNumbers <deanlaurea@icloud.com>
2015-10-22 12:15:40 -0500

    Play with size classes

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"), let weatherDictionary = NSDictionary(contentsOfFile: plistPath), let CurrentWeatherDictionary = weatherDictionary["currenlty"] as? [String: AnyObject] {
+        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
+            let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
+            let CurrentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject] {
         
             let currentWeather = CurrentWeather(weatherDictionary: CurrentWeatherDictionary)
         
